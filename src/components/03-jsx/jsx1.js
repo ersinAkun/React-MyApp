6ebde883bf9d1e5b11d59cee1e0f0187e3bc084d
Ersin Akun <ersinakun34@gmail.com>
2023-01-04 23:44:50 +0300

    v3

--- src/components/03-jsx/jsx1.js
+++ src/components/03-jsx/jsx1.js
@@ -1,11 +1,16 @@
-
-
-import React from 'react'
+import React from "react";
 
 const Jsx1 = () => {
-  return (
-    <div>Jsx1</div>
-  )
-}
 
-export default Jsx1
+    const message = 
+        "Class isimleri className içinde verilir ve attribute lar camelCase olarak yaızılır";
+
+  return (
+    <ul>
+      <li>Tüm elementler tek bir root element içinde olmalı</li>
+      <li className="title">{message}</li>
+    </ul>
+  );
+};
+
+export default Jsx1;
---
+++ src/components/03-jsx/jsx4.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Jsx4 = () => {
+  return (
+    <div>Jsx4</div>
+  )
+}
+
+export default Jsx4;
