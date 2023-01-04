831d3a07389ba2129bcc0554d631c171896ad4b5
Ersin Akun <ersinakun34@gmail.com>
2023-01-04 22:36:52 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import HelloWorld from "./components/01-hello-world/hello-world";
+import Jsx1 from "./components/03-jsx/jsx1";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         HELLO EVERYBODY
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div> <HelloWorld/>
+          <Jsx1/>
+   </div>
 
+  )
+};
 export default App;
+
+
