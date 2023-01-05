3f00e933613f0042f7326ba73f81c7c4480766d7
Ersin Akun <ersinakun34@gmail.com>
2023-01-05 21:35:03 +0300

    v1

--- src/components/03-jsx/jsx5.js
+++ src/components/03-jsx/jsx5.js
@@ -1,12 +1,29 @@
 import React from 'react'
+
+
 const Jsx5 = () => {
     const names = ["Ali", "Veli", "Ayşe", "Fatma"];
+  const cities = ["Istanbul", "Izmir", "London", "Paris", "Zurih"];
+
+
   return (
     <div>
         <ul>
-            { names.map( (x,i) => <li key={i}>{x}</li> ) }
+            { names.map( (x,i) => (
+            <li key={i}>{x}</li> 
+            )) }
             
         </ul>
+
+            <select>
+              { cities.map( (city, i)=> <option key={i} >{city}</option>  ) }
+
+
+              
+
+              </select>
+
+
     </div>
   )
 }
