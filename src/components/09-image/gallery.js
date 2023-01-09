b215c8d5ed1baf502e347200406bd90fd7966d19
Ersin Akun <ersinakun34@gmail.com>
2023-01-09 22:46:39 +0300

    v3

--- src/components/09-image/gallery.js
+++ src/components/09-image/gallery.js
@@ -1,19 +1,15 @@
 import React from "react";
 import "./gallery.scss";
 import images from "./images.json";
-
-
+const c = "gallery";
 const Gallery = () => {
   return (
-
-    <div className="gallery">
-
+    <div className={`${c} row`}>
       {images.map((img, i) => (
         <div key={i}>
-          <img src={require(`../../assets/img/${img.name}`)} alt="" />
+          <a href={require(`../../assets/img/${img.name}`)} target="_blank"><img src={require(`../../assets/img/${img.name}`)} alt="" /></a>
         </div>
       ))}
-      
     </div>
   );
 };
