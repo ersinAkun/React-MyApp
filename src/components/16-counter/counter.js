52dc40886a717199ec1b5f38eb0e6b7301b39d6e
Ersin Akun <ersinakun34@gmail.com>
2023-01-11 23:19:06 +0300

    v3

--- src/components/16-counter/counter.js
+++ src/components/16-counter/counter.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import { Button, ButtonGroup } from "react-bootstrap";
 import { AiOutlineMinusCircle, AiOutlinePlusCircle} from "react-icons/ai";
 import {GrPowerReset} from "react-icons/gr";
+
+
 const Counter = () => {
+    const [counter, setCounter] = useState(0);
+
   return (
+
     <ButtonGroup aria-label="Basic example">
-      <Button variant="info"><AiOutlinePlusCircle/></Button>
-      <Button variant="light" disabled>Middle</Button>
+      <Button variant="info" onClick={()=> setCounter(5)}><AiOutlinePlusCircle/></Button>
+      <Button variant="light" disabled>{counter}</Button>
       <Button variant="warning"><AiOutlineMinusCircle/></Button>
       <Button variant="danger"><GrPowerReset/></Button>
     </ButtonGroup>
+
   );
 };
 export default Counter;
