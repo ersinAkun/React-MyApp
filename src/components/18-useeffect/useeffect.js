7d2110d44deaa23f42905c5c5fc81a5650d4b7f2
Ersin Akun <ersinakun34@gmail.com>
2023-01-12 22:34:07 +0300

    v3

--- src/components/18-useeffect/useeffect.js
+++ src/components/18-useeffect/useeffect.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap';
-
 
 
 const UseEffect = () => {
@@ -8,16 +7,23 @@ const UseEffect = () => {
     const [message, setMessage] = useState("");
     const [error, setError] = useState("");
 
+    console.log("Bu satır render ve re-render durumlarında ve alttaki return kısmı render edilmeden önce çalıştırılır");
 
+
+    useEffect( ()=> {
+        console.log(`
+            MOUNTING: Bu satır sadece render durumunda çalışır.
+            Ancak re-render larda çalışmaz.
+        `)
+    }, []);
+
+    
   return (
-
     <div>
         <div>{message} - {error}</div>
         <Button variant="info" onClick={()=>setMessage("Hello")}>Set Message</Button>
         <Button variant="danger" onClick={()=>setError("Some errors")}>Throw Error</Button>
-
     </div>
-    
   )
 }
 export default UseEffect
