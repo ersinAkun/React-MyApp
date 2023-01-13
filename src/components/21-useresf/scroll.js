c3d9e869945f7106a2cd5082323f41ab1f6800ce
Ersin Akun <ersinakun34@gmail.com>
2023-01-13 21:56:33 +0300

    v3

--- src/components/21-useresf/scroll.js
+++ src/components/21-useresf/scroll.js
@@ -1,14 +1,35 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { Container } from "react-bootstrap";
 import "./scroll.scss";
-
-
 const Scroll = () => {
   const refAboutUs = useRef(null);
-  console.log(refAboutUs);
-
-  return (
+  
+  
+  useEffect(() => {
+    const handleScroll = () => { 
+        const aboutUsEl = refAboutUs.current;
+        const currentScrollPosition = window.scrollY;
     
+        const elScrollPosition = aboutUsEl.offsetTop;
+        const currentWindowHeight = window.innerHeight;
+        const elCurrentHeight = aboutUsEl.offsetHeight;
+    
+        const targetScrollPosition =
+          elScrollPosition - currentWindowHeight + elCurrentHeight;
+    
+    
+        if (currentScrollPosition >= targetScrollPosition) {
+            aboutUsEl.classList.add("active");
+        }
+    }
+    document.addEventListener("scroll", handleScroll);
+  
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    }
+  }, [])
+  
+  return (
     <Container>
       {[...new Array(15)].map((item, index) => (
         <p key={index}>
