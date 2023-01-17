97e87f255c881e418faaceb2601da1acee25f62a
Ersin Akun <ersinakun34@gmail.com>
2023-01-17 22:18:46 +0300

    v3

---
+++ src/components/26-forms/form6.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button, Container, Form } from "react-bootstrap";
+
+
+
+const Form6 = () => {
+  return (
+    <Container className="mt-5">
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+export default Form6;
