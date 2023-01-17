52777800fe30f7751a6cdc8f829c9252acb8abf0
Ersin Akun <ersinakun34@gmail.com>
2023-01-17 21:54:15 +0300

    v2

--- src/components/26-forms/form5.js
+++ src/components/26-forms/form5.js
@@ -1,27 +1,44 @@
-import React, { useState } from 'react'
-import { Button, Container, Form } from 'react-bootstrap'
-
-
+import React, { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
 const Form5 = () => {
-    const [formData, setFormData] = useState({
-        firstName:"",
-        lastName:"",
-        email:"",
-        phone:""
-    });
-
-
-    console.log(formData);
-
-    const handleFormData = (e) => {
-      const {name, value} = e.target;
-      setFormData({...formData, [name]: value })
-
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: ""
+  });
+  const handleFormData = (e) => {
+    const { name, value } = e.target;
+    setFormData({...formData, [name]: value});
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      if(!formData.firstName) throw new Error("Adınızı giriniz");
+      if(!formData.lastName) throw new Error("Soyadınızı giriniz");
+      if(!formData.email) throw new Error("Epostanızı giriniz");
+      if(!formData.phone) throw new Error("Telefonunuzu giriniz");
+      
+      setTimeout( ()=> {
+        alert("Bilgileriniz gönderildi");
+        setFormData({
+          firstName: "",
+          lastName: "",
+          email: "",
+          phone: ""
+        })
+      }, 1000);
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
     }
     
+    
+    
+  }
   return (
     <Container className="mt-5">
-      <Form method="post">
+      <Form method="post" onSubmit={handleSubmit} noValidate >
         <Form.Group className="mb-3">
           <Form.Label>İsim</Form.Label>
           <Form.Control
@@ -45,17 +62,17 @@ const Form5 = () => {
         <Form.Group className="mb-3">
           <Form.Label>Email</Form.Label>
           <Form.Control
-          name="email"
+            name="email"
             type="email"
             placeholder="Epostanızı giriniz"
             value={formData.email}
             onChange={(e) => handleFormData(e)}
           />
         </Form.Group>
-        <Form.Group>
+        <Form.Group className="mb-3">
           <Form.Label>Telefon</Form.Label>
           <Form.Control
-          name="phone"
+            name="phone"
             type="text"
             placeholder="Telefonunuzu giriniz"
             value={formData.phone}
@@ -65,6 +82,6 @@ const Form5 = () => {
         <Button variant="info" type="submit">Kaydet</Button>
       </Form>
     </Container>
-  )
-}
-export default Form5
+  );
+};
+export default Form5;
