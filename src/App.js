1102af39572d6fc445ec51309262b8e513b22351
Ersin Akun <ersinakun34@gmail.com>
2023-01-17 21:21:32 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ import Form1 from "./components/26-forms/form1";
 import Form2 from "./components/26-forms/form2";
 import Form3 from "./components/26-forms/form3";
 import Form4 from "./components/26-forms/form4";
+import Form5 from "./components/26-forms/form5";
 
 
 
@@ -95,10 +96,10 @@ const App = () => {
           <Form1/>          
           <Form2/>  
           <Form3/>
+          <Form4/>
           
         */}   
-                    <Form4/>
-
+            <Form5/>
 
    </div>
 
