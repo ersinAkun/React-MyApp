90101fd55d6dc95e62f0ffb6fa7ac687f89c869d
Ersin Akun <ersinakun34@gmail.com>
2023-01-18 21:50:58 +0300

    v2

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+//bos bir merkezi state olusturuldu 
+const StoreContext = createContext();
+
+
+//Merkezi state disariya acildi
+export default StoreContext;
